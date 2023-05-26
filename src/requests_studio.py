1368441c517a6a5a3ebef53705ae3f57d2e9a5fa
bissakov <78418192+bissakov@users.noreply.github.com>
2023-05-26 15:20:00 +0600

    Update requests_studio.py

--- src/requests_studio.py
+++ src/requests_studio.py
@@ -1,11 +1,12 @@
-from os import stat
 import requests
-from requests import Response
 import rich
 from typing import Dict, Optional, Any
 
 
 class Requests:
+    def __init__(self):
+        pass
+
     @staticmethod
     def request(method: str, url: str, params: Optional[Dict] = None, data: Optional[Dict] = None,
                 json: Optional[Dict] = None, headers: Optional[Dict] = None, cookies: Optional[Dict] = None,
@@ -13,11 +14,14 @@ class Requests:
         response = requests.request(method=method, url=url, params=params,
                                 data=data, json=json, headers=headers, cookies=cookies,
                                 timeout=timeout)
-        response.raise_for_status()
+        # response.raise_for_status()
         try:
-            return response.json()
+            res = response.json()
         except requests.exceptions.JSONDecodeError:
-            return None
+            res = None
+        if not res:
+            response.raise_for_status()
+        return res
 
     @staticmethod
     def get(**kwargs: Any) -> Optional[Dict]:
@@ -52,7 +56,32 @@ if __name__ == '__main__':
     # _requests = Requests()
     # rich.print(dir(requests))
     # r = Requests.get(url='https://reqres.in/api/users?page=1')
-    r = Requests.post(url='https://reqres.in/api/users', json={'name': 'morpheus', 'job': 'leader'})
-    # r = Requests.get('https://reqres.in')
-    rich.print(r)
+    # r = Requests.post(url='https://reqres.in/api/users', json={'name': 'morpheus', 'job': 'leader'})
+    # rich.print(r)
+
+    url = 'https://reqres.in/api/login'
+
+    payload = {
+        'username': 'test',
+        'email': 'test',
+        'password': 'test'
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://reqres.in/api-docs/',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Cookie': '__stripe_mid=e3839eff-eff6-407a-8017-8ce5edb15b1b8edfaf',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache'
+    }
+
+    res = Requests.post(url=url, headers=headers, json=payload)
+    print(res)
 
